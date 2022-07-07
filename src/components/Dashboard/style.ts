8979f3817f74a760cafe0daa3ff36a9ec87f51bd
nathanoliveira23 <nathanoliveira399@gmail.com>
2022-07-07 12:03:11 -0300

    adicionando responsividade

--- src/components/Dashboard/style.ts
+++ src/components/Dashboard/style.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
     display: grid;
     grid-template-columns: 40.5rem 1fr;
 
-    @media screen and (max-width: 768px) {
+    @media screen and (max-width: 980px) {
         display: block;
     }
 `
