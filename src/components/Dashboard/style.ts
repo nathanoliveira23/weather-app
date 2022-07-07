329a6dd31c150a9728e3dd3304a202075d5a86ed
nathanoliveira23 <nathanoliveira399@gmail.com>
2022-07-07 11:26:18 -0300

    adicionando responsividade

--- src/components/Dashboard/style.ts
+++ src/components/Dashboard/style.ts
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: grid;
     grid-template-columns: 40.5rem 1fr;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+    }
 `
